b460311a0c0ee9f6509eae571bd974bbdd460b9a
Gustin Florian <florian.gustin.business@gmail.com>
2019-09-10 22:40:11 +0200

    treerr

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_calories_counter/widgets/custom_text.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,35 +9,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Calories Counter',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(title: 'Calories Counter Demo Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -44,68 +29,202 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  bool switchItem = false;
+  DateTime dateTimeItem;
+  double sliderItem = 15.0;
+
+  Color type = Colors.red;
+
+  String textItem;
+  String textSubmit;
+
+  int selectActivity;
+  List<String> activityList = [
+    "Low",
+    "Medium",
+    "High"
+  ];
+
+  List<Widget> activityStatus() {
+    List<Widget> l = [];
+    for(int i = 0; i < activityList.length; i++) {
+      Column column = Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          Radio(
+            activeColor: type,
+              value: i,
+              groupValue: selectActivity,
+              onChanged: (int i) {
+                setState(() {
+                  selectActivity = i;
+                });
+              }
+          ),
+          CustomText(activityList[i], color: type,)
+        ],
+      );
+      l.add(column);
+    }
+    return l;
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).requestFocus(FocusNode());
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: CustomText("Calories Counter Demo Page", color: Colors.white,),
+          backgroundColor: type,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CustomText("Remplissez tous les champs pour obtenir votre besoin journalier en calories"),
+              SizedBox(
+                height: 20.0,
+              ),
+              Card(
+                child: Container(
+                  height: MediaQuery.of(context).size.height/1.5,
+                  padding: EdgeInsets.all(20.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          CustomText("Femme", color: Colors.red,),
+                          Switch(
+                              value: switchItem,
+                              activeColor: Colors.blue,
+                              inactiveTrackColor: Colors.red,
+                              onChanged: (bool b) {
+                                if(b) {
+                                  type = Colors.blue;
+                                }else {
+                                  type = Colors.red;
+                                }
+                                setState(() {
+                                  switchItem = b;
+                                });
+                              }
+                          ),
+                          CustomText("Homme", color: Colors.blue,),
+                        ],
+                      ),
+                      RaisedButton(
+                        child: CustomText("Votre age : ${(dateTimeItem!=null)? dateTimeItem.toString() : ''}", color: Colors.white, weight: FontWeight.bold,),
+                          color: type,
+                          onPressed: showBirthDate
+                      ),
+                      CustomText("Votre taille : ${sliderItem.toInt()*10} cm", color: type, factor: 1.2,),
+                      Slider(
+                          value: sliderItem,
+                          min: 15.0,
+                          max: 21.0,
+                          inactiveColor: (type==Colors.red)? Colors.red[100] : Colors.blue[100],
+                          activeColor: type,
+                          onChanged: (double d) {
+                            setState(() {
+                              sliderItem = d;
+                            });
+                          }
+                      ),
+                      TextField(
+                        keyboardType: TextInputType.number,
+                        onChanged: (String s) {
+                          textItem = s;
+                        },
+                        onSubmitted: (String s) {
+                          textItem = s;
+                          setState(() {
+                            textSubmit = s;
+                          });
+                        },
+                        decoration: InputDecoration(
+                          border: UnderlineInputBorder(),
+                          labelText: "Entrez votre poids en kilos",
+                        ),
+                      ),
+                      CustomText("Quelle est votre activité sportive ?", color: type, factor: 1.2,),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: activityStatus(),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              RaisedButton(
+                child: CustomText("Calculate", color: Colors.white, weight: FontWeight.bold,),
+                  color: type,
+                  onPressed: checkFieldStatusAll,
+              )
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  Future<Null> showBirthDate() async {
+    DateTime select = await showDatePicker(
+        context: context,
+        initialDate: DateTime.now(),
+        initialDatePickerMode: DatePickerMode.year,
+        firstDate: DateTime(1900),
+        lastDate: DateTime(2050),
+    );
+    if(select!=null) {
+      setState(() {
+        dateTimeItem = select;
+      });
+    }
+  }
+
+  Future<Null> showEstimation() async {
+    return showDialog(
+        context: context,
+      barrierDismissible: false,
+      builder: (BuildContext estimationContext) {
+          return SimpleDialog(
+            backgroundColor: Colors.white,
+
+            children: <Widget>[
+
+            ],
+          );
+      }
+    );
+  }
+
+  Future<Null> showError() async {
+    return showDialog(
+        context: context,
+      barrierDismissible: true,
+      builder: (BuildContext errorContext) {
+          return AlertDialog(
+            backgroundColor: Colors.red,
+            title: CustomText("All fields are required", color: Colors.white,),
+          );
+      }
+    );
+  }
+
+  void checkFieldStatusAll() {
+    if(switchItem!=null && dateTimeItem!=null && sliderItem!=null && textItem!=null && selectActivity!=null){
+      showEstimation();
+    }else {
+      showError();
+    }
   }
 }
